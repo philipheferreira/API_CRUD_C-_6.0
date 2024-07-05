e473ccbaa26e81a357be917745718bf8b0f1166a
philipheferreira <philipheferreira@gmail.com>
2024-07-05 11:08:52 -0300

    Feet: pasts and entity

--- APICRUD/Controllers/UsuarioController.cs
+++ APICRUD/Controllers/UsuarioController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using APICRUD.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APICRUD.Controllers{
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/[controller]")] // comando para geração da rota que vai ser acessada
+    [ApiController] // inicializa o controler
     public class UsuarioController : ControllerBase{
+        [HttpGet]//gera a inicialização para o swagger
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios(){ // Criação do nosso primeiro EndPoint que cria uma ação que resulta na geração de uma lista acessando o UsuarioModel
+            return Ok(); // retorna sempre 200
+        }
     }
 }
