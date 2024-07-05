0a1268462b0a6ddcb69fe9d0a3c30c62b314d44c
philipheferreira <philipheferreira@gmail.com>
2024-07-05 10:36:57 -0300

    Feet: create model usuario and tarefas

---
+++ APICRUD/Controllers/UsuarioController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICRUD.Controllers{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase{
+    }
+}
